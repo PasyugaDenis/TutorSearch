22d4733c3f1cbd5f938ab98104bd61eac92cd567
Andrew <andrew.yehozha@gmail.com>
2019-05-05 15:48:42 +0300

    [task_2.2] Fix GetList method
    - Add view model for course
    - Add field "Id" in teacher view model

---
+++ TutorSearch/TutorSearch.Web/Models/Response/CourseViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TutorSearch.Web.Models.Response
+{
+    public class CourseViewModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Specialty { get; set; }
+
+        public bool IsClosed { get; set; }
+
+        public string Description { get; set; }
+    }
+}
