bd8e13016101baa390a71390f139878c26fb718e
Andrew <andrew.yehozha@gmail.com>
2019-05-05 12:48:33 +0300

    [task_3.1] Add validation of registration data
    - Add Name check
    - Add Surname check
    - Add email check
    - Add password check
    - Сhange route and ViewProfile methods

--- TutorSearch/TutorSearch.Web/Models/Request/AuthorizationRequest.cs
+++ TutorSearch/TutorSearch.Web/Models/Request/AuthorizationRequest.cs
@@ -1,9 +1,16 @@
-﻿namespace TutorSearch.Web.Models.Request
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TutorSearch.Web.Models.Request
 {
     public class AuthorizationRequest
     {
+
+        [Required(ErrorMessage = "The email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "Enter your Password.")]
+        [StringLength(50, MinimumLength = 7, ErrorMessage = "Password should be longer than 6 characters.")]
         public string Password { get; set; }
     }
 }
