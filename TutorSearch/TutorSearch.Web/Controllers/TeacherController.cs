70257efa46057edb5539f81d2c94b6e872b318d1
Andrew <andrew.yehozha@gmail.com>
2019-05-20 13:08:44 +0300

    [task_3.6] Add TeacherPageViewModel
    - Add TeacherPageViewModel

--- TutorSearch/TutorSearch.Web/Controllers/TeacherController.cs
+++ TutorSearch/TutorSearch.Web/Controllers/TeacherController.cs
@@ -176,20 +176,20 @@ namespace TutorSearch.Web.Controllers
                 WhatsUp = teacher.Contacts.WhatsUp
             };
 
-            var result = new TeacherViewModel
+            var result = new TeacherPageViewModel
             {
                 Id = teacher.User.Id,
                 Name = teacher.User.Name,
                 Surname = teacher.User.Surname,
-                Birthday = teacher.User.Birthday,
+                Age = ((DateTime.Now - teacher.User.Birthday).Days / 365),
                 Email = teacher.User.Email,
-                Phone = teacher.User.Phone,
                 Education = teacher.Education,
                 Skill = teacher.Skill,
                 City = teacher.City,
-                IsPrivate = teacher.IsPrivate,
                 WorkExperience = teacher.WorkExperience,
-                IsTeacher = teacher.User.IsTeacher,
+                Description = teacher.Description,
+                IsPrivate = teacher.IsPrivate,
+                Phone = teacher.User.Phone,
                 Contacts = contacts
             };
 
